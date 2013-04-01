ececc19265ccb1c9ce267f6d6f495abbd2dc31df
bossman759 <bossman759@gmail.com>
2013-04-01 20:27:15 -0300

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>NXT</title>
+  </head>
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+<body>
+<style>
+@iconSpritePath: asset-path('twitter/bootstrap/glyphicons-halflings.png');
+</style>
+<div class="navbar">
+  <div class="navbar-inner">
+    <ul class="nav">
+      <li class="active"><a href="http://nxt.comxa.com/"><img src="http://nxt.comxa.com/images/glyphicons_020_home.png"> Home</a></li>
+      <li><a href="http://nxt.comxa.com/submit"><img src="http://nxt.comxa.com/images/glyphicons_150_edit.png"> Sumbit</a></li>
+    </ul>
+    <form class="navbar-search pull-right" method='get' action='search'>
+  <input type="text" name='search' class="search-query" placeholder="Search...">
+</form>
+  </div>
+</div>
+
+
+<div class="page-header">
+  <h1>NXT<small> The worlds information.</small></h1>
+</div>
+<table class="table table-striped" cellspacing="10px">
+  <caption></caption>
+  <thead>
+    <tr class="success">
+      <th><img src="/images/glyphicons_049_star.png"></img> About</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+
+  <center><h1>404 Page Not Found...</h1></center><br />
+	
+      	</td>
+    </tr>
+  </tbody>
+</table>
+<div class="alert alert-info">
+NXT 2013 - Mundi informationes<br />
+<a target='_blank' href='http://thenxt.tumblr.com/'>Blog</a> | <a target='_blank' href='http://twitter.com/asknxt'>@AskNXT</a> | <a href='https://github.com/bossman759/nxt' target='_blank'>Github</a>
+</div>
+</body>
+</html>
