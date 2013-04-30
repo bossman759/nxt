af2735f7fd5d48fc987f299e1be0e0ace436eb65
bossman759 <bossman759@gmail.com>
2013-04-30 16:29:35 -0700

    Updated Examples/search.py

--- Examples/search.py
+++ Examples/search.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 #import need packages
 import sqlite3 as lite
-import sys
-import urllib
-import BeautifulSoup
 
 #create a connection with the database file from our data dumps
 con = lite.connect('test.db')
@@ -27,21 +24,13 @@ else:
 with con:
     #create our cursor
     cur = con.cursor()               
-        #SQL query for search through Def table
-        cur.execute("SELECT * FROM Def WHERE Word LIKE ? OR Def LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
-        rows = cur.fetchall()
-        
-        for row in rows:
-            #printing second & third column(See tuples)
-            print row[1]
-            
-            print row[2]
+        #SQL query for search through Def tabl
             
             #SQL query for search through Links table
-        cur.execute("SELECT * FROM Links WHERE url LIKE ? OR Name LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
-        rows = cur.fetchall()
+    cur.execute("SELECT * FROM Links WHERE url LIKE ? OR Name LIKE ? LIMIT 12", ('%'+t+'%', '%'+t+'%'))
+    rows = cur.fetchall()
         
-        for row in rows:
+    for row in rows:
             #printing second & third column(See tuples)
             print row[1]
             
