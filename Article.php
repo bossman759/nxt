729875ecb61de42a81bd2ef0b65e919d007e7098
Officialnxt <esmith233@textfree.us>
2013-05-06 14:35:58 -0700

    Updated Article.php

--- Article.php
+++ Article.php
@@ -1,54 +1,4 @@
-<!DOCTYPE HTML>
-<html>
-  <head>
-  </head>
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-  <script type="text/javascript" src="js/bootstrap.js"></script>
-<script type="text/javascript">
-var timeOut;
-function scrollToTop() {
-  if (document.body.scrollTop!=0 || document.documentElement.scrollTop!=0){
-    window.scrollBy(0,-50);
-    timeOut=setTimeout('scrollToTop()',10);
-  }
-  else clearTimeout(timeOut);
-}
-</script>
-<body>
-<style>
-@iconSpritePath: asset-path('twitter/bootstrap/glyphicons-halflings.png');
-</style>
-<div class="navbar">
-  <div class="navbar-inner">
-    <ul class="nav">
-      <li><a href="http://nxt.comxa.com/"><img src="http://nxt.comxa.com/images/glyphicons_020_home.png"> Home</a></li>
-      <li><a href="http://nxt.comxa.com/submit"><img src="http://nxt.comxa.com/images/glyphicons_150_edit.png"> Sumbit</a></li>
-    </ul>
-    <form class="navbar-search pull-right" method='get' action='search'>
-  <input type="text" name='search' class="search-query" placeholder="Search...">
-</form>
-  </div>
-</div>
-
-
-<div class="page-header">
-  <h1>NXT<small> The world's information.</small></h1>
-</div>
-<table class="table table-bordered" cellspacing="10px">
-  <caption></caption>
-  <thead>
-    <tr class="success">
-      <th><img src="http://nxt.comxa.com/images/glyphicons_150_edit.png"></img> Articles</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>
-	<?php
+<?php
 	if($_GET['id']){
 	require("connect.php");
 	$id = $_GET['id'];
@@ -150,13 +100,3 @@ function scrollToTop() {
 	}
 	}
 	?>
-     </td>
-    </tr>
-  </tbody>
-</table>
-<div class="alert alert-info">
-NXT 2013 - Mundi informationes<br />
-<a target='_blank' href='http://thenxt.tumblr.com/'>Blog</a> | <a target='_blank' href='http://twitter.com/asknxt'>@AskNXT</a> | <a href='http://officialnxt.github.io/nxt' target='_blank'>Github</a> | <a href="#" onclick="scrollToTop();return false">Top</a>
-</div>
-</body>
-</html>
