0a907d0ed75a67c0ac61d2612fbc0a91ae29125b
Officialnxt <esmith233@textfree.us>
2013-05-06 14:35:25 -0700

    Updated Spam.php

--- Spam.php
+++ Spam.php
@@ -1,114 +1,19 @@
-<!DOCTYPE HTML>
-<html>
-  <head>
-    <title>NXT</title>
-  </head>
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-  <script type="text/javascript" src="js/bootstrap.js"></script>
-<body>
-<style>
-@iconSpritePath: asset-path('twitter/bootstrap/glyphicons-halflings.png');
-</style>
-<div class="navbar">
-  <div class="navbar-inner">
-    <ul class="nav">
-      <li><a href="http://nxt.comxa.com/"><img src="http://nxt.comxa.com/images/glyphicons_020_home.png"> Home</a></li>
-      <li><a href="http://nxt.comxa.com/submit"><img src="http://nxt.comxa.com/images/glyphicons_150_edit.png"> Sumbit</a></li>
-    </ul>
-    <form class="navbar-search pull-right" method='get' action='search'>
-  <input type="text" name='search' class="search-query" placeholder="Search...">
-</form>
-  </div>
-</div>
-
-
-<div class="page-header">
-  <h1>NXT<small> The world's information.</small></h1>
-</div>
-<table class="table table-bordered" cellspacing="10px">
-  <caption></caption>
-  <thead>
-    <tr class="success">
-      <th><img src="http://nxt.comxa.com/images/glyphicons_150_edit.png"></img> Sumbit an Article</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>
-      <form method='post'>
-	<input type='text' name='title' placeholder='Title...' required><br />
-	<textarea name='text' rows='7' placeholder='Text...' required></textarea><br />
-	<input type='submit' name='submit' class='btn btn-success'>
-      </form>
 	<?php
+	if(strip_tags($_GET['id'])){
+	echo "<form action='' method='post'>
+	<input type='submit' name='submit' value='Spam?' class='btn btn-success'>
+ 	</form>";
+
+	if($_POST['submit']){
 	require("connect.php");
-	$title = mysql_real_escape_string(stripslashes(strip_tags($_POST['title'])));
-	$text = mysql_real_escape_string(stripslashes(strip_tags($_POST['text'])));
+	$id = strip_tags(stripslashes($_GET['id']));
+	mysql_query("DELETE FROM Links WHERE id='$id'")or die(mysql_error());
 
-	if($title){
-
-		if($text){
-		
-			$sql = mysql_query("SELECT * FROM Articles WHERE title='$title' AND text='$text'");
-			$numrows = mysql_num_rows($sql);
-			if(numrows == 0){
-
-			$date = date("M d, Y");
-
-	
-			mysql_query("INSERT INTO Articles VALUES('', '$title', '$text', '$date')");				
-			
-			$sql = mysql_query("SELECT * FROM Articles WHERE title='$title' AND text='$text'");
-			$numrows = mysql_num_rows($sql);
-			if(numrows == 0){
-
-				header("Location: http://nxt.comxa.com");
-			
-			}
-			else
-				echo "<center><div class='alert alert-error'>
-				An error occurred
-				Your article was not
-				submitted sadly
-				</div></center>";
-
-			}
-			else
-				echo "<center><div class='alert alert-error'>
-				This article has<br />
-				Been sumbitted already<br />
-				Please don't plagiarise
-				</div></center>";
-
-		}
-		else
-			echo "<center><div class='alert alert-error'>
-			You must submit some<br />
-			Text for your article<br />
-			Please try adding some
-			</div></center>";
+	header("LOCATION: http://nxt.comxa.com/");
 
 	}
-	else
-		echo "<center><div class='alert alert-error'>
-		You must submit a<br />
-		Name for your article<br />
-		Please try adding one
-		</div></center>";
 
+	}else{
 
-	?>
-     </td>
-    </tr>
-  </tbody>
-</table>
-<div class="alert alert-info">
-NXT 2013 - Mundi informationes<br />
-<a target='_blank' href='http://thenxt.tumblr.com/'>Blog</a> | <a target='_blank' href='http://twitter.com/asknxt'>@AskNXT</a> | <a href='https://github.com/bossman759/nxt' target='_blank'>Github</a>
-</div>
-</body>
-</html>
+	}
+?>
